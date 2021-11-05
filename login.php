d38220310beb342faa8001d3d7860ca0d8b06d5d
samkelozuma66 <samkelozuma66@gmail.com>
2021-11-05 15:13:51 +0200

    Section 2

---
+++ login.php
@@ -0,0 +1,197 @@
+<?php
+
+include 'config.php';
+include 'error.php';
+
+  if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if(empty($_POST['email'])){
+      $emailErr = "please input email";
+    }
+    elseif(empty($_POST['password'])){
+      $passErr = "password required";    
+    }               
+    else{            
+          $user = $conn ->getRow('user',['email'=>$_POST['email']]);      
+          
+          if(count($user)<=0){
+            $emailErr = "Sorry!! you are not registered"; 
+          }   
+        }  
+    if(isset($user[0])){
+        if($user[0]['password']==md5($_POST['password'])){
+          
+          $_SESSION['id']=$user[0]['id'];
+          $_SESSION['first_name']=$user[0]['firstname'];
+          $_SESSION['last_name']=$user[0]['lastname'];
+          $_SESSION['email']=$user[0]['email']; 
+
+          if(isset($_POST['remeberme']))
+          {
+            echo "<script>localStorage.setItem('email', '".$_POST['email']."');</script>";
+            echo "<script>localStorage.setItem('password', '".$_POST['password']."');</script>";
+            //echo "<script>alert('".$_POST['remeberme']."')</script>";
+          }
+          
+          echo "<script>window.location.href='profile.php'</script>";
+          /*//print_r($_SESSION); die;  remeberme localStorage.setItem("lastname", "Smith");
+          if($_SESSION['id_number']== null){
+            echo "<script>window.location.href='registerUpdate.php'</script>";
+          } 
+          elseif($_SESSION['status']!='approved'){
+            echo "<script>window.location.href='registerUpdate.php'</script>";
+          }elseif($_SESSION['regType']=='individual')
+          {
+            echo "<script>window.location.href='model/index.php'</script>";
+          }elseif($_SESSION['regType']=='company'){
+            echo "<script>window.location.href='studio/index.php'</script>";
+          }    */     
+        }else{
+          $passErr = "Invalid password!!!!"; 
+        }
+        
+    }
+  }          
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Required meta tags -->
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" type="image/png" sizes="16x16" href="images/favicon1.png">
+    <title>Pondacam </title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="https://www.pondacams.com/assets/css/materialdesignicons.min.css">
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="https://www.pondacams.com/assets/css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+	
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+	
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.6.55/css/materialdesignicons.min.css">
+    <!-- End layout styles -->
+    <style>
+      .error{
+        color:red;
+      }
+    </style>
+  <script type="text/javascript">
+    var email = localStorage.getItem("email");
+    var password = localStorage.getItem("password");
+
+    if (email !== "" && email !== null) 
+    {
+      if (password !== "" && password !== null) 
+      {
+        //alert("Credetials Save");
+      }
+    }
+  </script>
+   
+  </head>
+  <body>
+    <div class="container-scroller">
+      <div class="container-fluid page-body-wrapper full-page-wrapper">
+        <div class="content-wrapper d-flex align-items-stretch auth auth-img-bg">
+          <div class="row flex-grow">
+            <div class="col-lg-6 d-flex align-items-center justify-content-center">
+              <div class="auth-form-transparent text-left p-3">
+                <div class="brand-logo" >
+                  <h1>LOGIN</h1>
+                </div>
+                <h4>Welcome back!</h4>
+                <h6 class="font-weight-light">Happy to see you again!</h6>
+                <form class="pt-3" method = "post"  >
+                  <div class="form-group">
+                    <label for="exampleInputEmail">Email</label>
+                    <div class="input-group">
+                      <div class="input-group-prepend bg-transparent">
+                        <span class="input-group-text bg-transparent border-right-0">
+                          <i class="mdi mdi-account-outline text-primary"></i>
+                        </span>
+                      </div>
+                      <input type="email" name = "email" id="email" class="form-control form-control-lg border-left-0"
+                      value = "<?php if(isset($_POST['email'])){echo $_POST['email']; } ?>" id="" placeholder="Username">
+                    </div>
+                    <div><span class = "error"><?php echo $emailErr?> </span></div>
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword">Password</label>
+                    <div class="input-group">
+                      <div class="input-group-prepend bg-transparent">
+                        <span class="input-group-text bg-transparent border-right-0">
+                          <i class="mdi mdi-lock-outline text-primary"></i>
+                        </span>
+                      </div>
+                      <input type="password" name="password" id="password" class="form-control form-control-lg border-left-0" id="exampleInputPassword"
+                      value = "<?php if(isset($_POST['password'])){echo $_POST['password']; } ?>" placeholder="Password">             
+                    </div>
+                    <div><span class = "error"><?php echo $passErr?> </span></div>
+                  </div>
+                  <div class="my-2 d-flex justify-content-between align-items-center">
+                    <div class="form-check">
+                      <label class="form-check-label text-muted">
+                        <input type="checkbox" name="remeberme"class="form-check-input"> Keep me signed in <i class="input-helper"></i></label>
+                    </div>
+                    <a href="reset-password.html" class="auth-link text-black">Forgot password?</a>
+                  </div>
+                  <div class="my-3">
+                    <button type = "submit" id="login" class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn" >LOGIN</button>
+                  </div>
+                  <div class="text-center mt-4 font-weight-light"> Don't have an account? 
+				          <a href="register.php" class="text-primary">Create</a>
+                  </div>
+                  
+                  <div class="text-center mt-4 font-weight-light">  
+				          <a href="index.php" class="text-primary">JOIN CHATS</a>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <style type="text/css">
+               .test{
+                  background: url(me.JPG);
+                  background-size: cover;
+                  background-repeat: no-repeat;
+                  background-position: 100% 35%;
+               }
+            </style>
+            <div class="col-lg-6 test d-flex flex-row">
+              <p class="text-white font-weight-medium text-center flex-grow align-self-end">Designed By SAMKELO ZUMA Copyright &copy; 2021 All rights reserved.</p>
+            </div>
+          </div>
+        </div>
+        <!-- content-wrapper ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    
+    <!-- container-scroller -->
+    <script type="text/javascript">
+      var email = localStorage.getItem("email");
+      var password = localStorage.getItem("password");
+
+      if (email !== "" && email !== null) 
+      {
+        if (password !== "" && password !== null) 
+        {
+          var inEmail = document.getElementById("email");
+          var inPassw = document.getElementById("password");
+          var btnLogin = document.getElementById("login");
+          inEmail.value = email;
+          inPassw.value =password;
+          btnLogin.click();
+          //alert("Credetials Save");
+        }
+      }
+    </script>
+    <script src="https://www.pondacams.com/assets/js/off-canvas.js"></script>
+    <script src="https://www.pondacams.com/assets/js/hoverable-collapse.js"></script>
+    <script src="https://www.pondacams.com/assets/js/misc.js"></script>
+    <script src="https://www.pondacams.com/assets/js/settings.js"></script>
+    <script src="https://www.pondacams.com/assets/js/todolist.js"></script>
+    <!-- endinject -->
+  
+</body>
+</html>
---
+++ profile.php
@@ -0,0 +1,208 @@
+<?php
+include 'config.php';
+include 'error.php';
+$error ="";
+$user_otp ="";
+$fnameErr = "";
+$done = 0;
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $error = true;
+    if(empty($_POST['first_name'])){
+        $fnameErr = "please input First name";                      
+        $error = false;
+    }elseif(!preg_match("/^[a-zA-Z0-9 ]*$/", $_POST['first_name'])){
+        $error = false;
+        $fnameErr = "only alphabet and white space allowed";
+    }
+    if(empty($_POST['last_name'])){
+        $nameErr = "please input Last name";                      
+        $error = false;
+    }elseif(!preg_match("/^[a-zA-Z0-9 ]*$/", $_POST['first_name'])){
+        $error = false;
+        $nameErr = "only alphabet and white space allowed";
+    }
+    $mail = $conn->getRow('user',['email'=>$_POST['email']]);
+    if(empty($_POST['email'])){
+        $error = false;
+        $emailErr = "input field required";
+    }elseif(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+        $error = false;
+        $emailErr = "Invalid email format";
+    }elseif(count($mail)>0){
+      $error = false;
+      $emailErr = "Email Already Exist!!!!";
+    }
+    /* echo "<pre>"; Password must have at least 1 uppercase character, 1 lowercase character, 1 special character,1 number and must be at least 6 characters long
+    print_r($mail);die; */
+    /* */
+    
+    if(empty($_POST['password'])){
+        $error = false;
+        $passErr = "password required";
+    
+    }elseif(empty($_POST['confirm_password'])){
+        $error = false;
+        $CpassErr = "please confirm password"; 
+    }elseif($_POST['password']!=$_POST['confirm_password']){
+        $error = false;
+        $passErr = "password do not match!!!";
+    }
+    else
+    {
+      $pass = $_POST['password'];
+      if(strlen($pass)<6)
+      {
+        $error = false;
+        $passErr = "Password must be at least 6 characters long ";
+      }
+      elseif (preg_match("/[A-Z]/", $pass)===0) 
+      {
+        $error = false;
+        $passErr = "Password must have at least 1 uppercase character";
+      }
+      elseif (preg_match("/[a-z]/", $pass)===0) 
+      {
+        $error = false;
+        $passErr = "Password must have at least 1 lowercase character";
+      }
+      elseif (preg_match("/[0-9]/", $pass)===0) 
+      {
+        $error = false;
+        $passErr = "Password must have at least 1 number";
+      }
+      elseif (preg_match("/[\'^£$%&*()}{@#~?><>,|=_+¬-]/", $pass)===0) 
+      {
+        $error = false;
+        $passErr = "Password must have at least 1 special character '\'^£$%&*()}{@#~?><>,|=_+¬-'";
+      }
+    }    
+  }
+if($error){ 
+    
+    $data = ['firstname'=>$_POST['first_name'],'lastname'=>$_POST['last_name'],'email'=>$_POST['email'],'password'=>md5($_POST['password'])];
+    $done = $conn ->insData('user',$data);
+         
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Required meta tags -->
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" type="image/png" sizes="16x16" href="images/favicon1.png">
+    <title>Pondacam | Signup</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="https://www.pondacams.com/assets/css/materialdesignicons.min.css">
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="https://www.pondacams.com/assets/css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+	
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+	
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.all.min.js"></script>
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.6.55/css/materialdesignicons.min.css">
+    <!-- End layout styles -->
+    <style>
+        .error{
+                color : red;
+            }
+        </style>
+  <?php
+  echo $done;
+    if($done){       
+      $_SESSION['name'] = $_POST['first_name'];
+      $_SESSION['email']=$_POST['email'];
+      echo "<script> swal.fire('HI! ".$_SESSION['name']." You have been registered.Email :".$_SESSION['email']."');</script>";
+     //echo "<script>window.location.href='verify_otp.php'</script>";
+    } 
+  ?>
+  </head>
+  <body>
+    <div class="container-scroller">
+      <div class="container-fluid page-body-wrapper full-page-wrapper">
+        <div class="content-wrapper d-flex align-items-stretch auth auth-img-bg">
+          <div class="row flex-grow">
+            <div class="col-lg-6 d-flex align-items-center justify-content-center">
+              <div class="auth-form-transparent text-left p-3">
+                <div class="brand-logo" >
+                  <h1>PROFILE</h1>
+                </div>
+                <h4>Welcome back! <?php if(isset($_SESSION['first_name'])){echo $_SESSION['first_name']; }?></h4>
+                <form class="pt-3" method = 'post'>
+                  <div class="form-group">
+                    <label for="exampleInputEmail">First Name</label>
+                    <div class="input-group">
+                      <div class="input-group-prepend bg-transparent">
+                        <span class="input-group-text bg-transparent border-right-0">
+                          <i class="mdi mdi-account-outline text-primary"></i>
+                        </span>
+                      </div>
+                      <input type="text" class="form-control form-control-lg border-left-0" name = "first_name" id="first_name"
+                       placeholder="Firstname" value = "<?php if(isset($_SESSION['first_name'])){echo $_SESSION['first_name']; }?>" disabled>
+                    </div>
+                    <span class = "error"><?php echo $fnameErr ?></span>
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputEmail">Last Name</label>
+                    <div class="input-group">
+                      <div class="input-group-prepend bg-transparent">
+                        <span class="input-group-text bg-transparent border-right-0">
+                          <i class="mdi mdi-account-outline text-primary"></i>
+                        </span>
+                      </div>
+                      <input type="text" class="form-control form-control-lg border-left-0" name = "last_name" id="last_name"
+                       placeholder="Lastname" value = "<?php if(isset($_SESSION['last_name'])){echo $_SESSION['last_name']; }?>" disabled>
+                    </div>
+                    <span class = "error"><?php echo $nameErr ?></span>
+                  </div>
+				          <div class="form-group">
+                    <label>Email</label>
+                    <div class="input-group">
+                      <div class="input-group-prepend bg-transparent">
+                        <span class="input-group-text bg-transparent border-right-0">
+                          <i class="mdi mdi-email-outline text-primary"></i>
+                        </span>
+                      </div>
+                      <input type="email" class="form-control form-control-lg border-left-0"name = "email" id="email"
+                      placeholder="email " value = "<?php if(isset($_SESSION['email'])){echo $_SESSION['email']; }?>" disabled>
+                    </div>
+                    <span class = "error"><?php echo $emailErr ?></span>
+                  </div>
+                  
+                  <!--<div class="my-3">
+                    <button type = "submit" name = "sendMail" class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn" >CREATE  ACCOUNT</button>
+                  </div>-->
+                  
+                </form>
+              </div>
+            </div>
+            <style type="text/css">
+               .test{
+                  background: url(me.JPG);
+                  background-size: cover;
+                  background-repeat: no-repeat;
+                  background-position: 100% 35%;
+               }
+            </style>
+            <div class="col-lg-6 test d-flex flex-row">
+              <p class="text-white font-weight-medium text-center flex-grow align-self-end">Designed By SAMKELO ZUMA Copyright &copy; 2021 All rights reserved.</p>
+            </div>
+          </div>
+        </div>
+        <!-- content-wrapper ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    
+    <script src="https://www.pondacams.com/assets/js/off-canvas.js"></script>
+    <script src="https://www.pondacams.com/assets/js/hoverable-collapse.js"></script>
+    <script src="https://www.pondacams.com/assets/js/misc.js"></script>
+    <script src="https://www.pondacams.com/assets/js/settings.js"></script>
+    <script src="https://www.pondacams.com/assets/js/todolist.js"></script>
+    <!-- endinject -->
+  
+</body>
+</html>
